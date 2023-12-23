433c3dd60113d848479a9fca9bebbc0f5d048370
nooks <nooks@qq.com>
2023-12-23 17:55:17 +0800

    Initial commit

---
+++ TravelMode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelMode
+//
+//  Created by Angel on 2023/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelMode/TravelModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelModeApp.swift
+//  TravelMode
+//
+//  Created by Angel on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
