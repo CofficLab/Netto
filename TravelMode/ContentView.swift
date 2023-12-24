5d4f7ffd7964bdf1138bbafe420c720c610b1975
nooks <nooks@qq.com>
2023-12-24 20:32:24 +0800

    增加swift ui项目

---
+++ TravelMode/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TravelMode
+//
+//  Created by Angel on 2023/12/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelMode/TravelModeApp.swift
@@ -0,0 +1,18 @@
+//
+//  TravelModeApp.swift
+//  TravelMode
+//
+//  Created by Angel on 2023/12/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TravelModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
