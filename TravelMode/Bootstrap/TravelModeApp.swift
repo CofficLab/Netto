aab007f01916dd3f035ec9e86286aa61c6317b36
Nookery <nooks@qq.com>
2025-05-26 22:44:49 +0800

    🎨 Chore:  Minor adjustments

--- TravelMode/Bootstrap/TravelModeApp.swift
+++ TravelMode/Bootstrap/TravelModeApp.swift
@@ -2,11 +2,36 @@ import SwiftUI
 
 @main
 struct TravelModeApp: App {
+    @StateObject private var app = AppManager()
+    @StateObject private var event = EventManager()
+    @StateObject private var channel = ChannelProvider()
+    @StateObject private var m = MessageProvider()
+    @StateObject private var p = PluginProvider()
+    
     var body: some Scene {
+//        MenuBarExtra("TravelMode", systemImage: "network") {
+//            Button("Show Window") {
+//                NSApplication.shared.activate(ignoringOtherApps: true)
+//            }
+//            .keyboardShortcut("w")
+//            
+//            Divider()
+//            
+//            Button("Quit") {
+//                NSApplication.shared.terminate(nil)
+//            }
+//            .keyboardShortcut("q")
+//        }
+        
         WindowGroup {
             RootView {
                 ContentView()
             }
+            .environmentObject(app)
+            .environmentObject(event)
+            .environmentObject(channel)
+            .environmentObject(m)
+            .environmentObject(p)
         }
         .commands(content: {
             DebugCommands()
