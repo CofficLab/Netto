d06f88c47d4299c004a1feed921241b3cbf61629
An <nooks@qq.com>
2025-09-08 15:53:20 +0800

    💄 UI: Remove deprecated Toolbar component and update layout

--- Core/View/Layout/ContentView.swift
+++ Core/View/Layout/ContentView.swift
@@ -12,18 +12,6 @@ struct ContentView: View {
         }
         .frame(maxWidth: .infinity)
         .navigationTitle("")
-        .toolbar {
-            ToolbarItem(placement: .navigation, content: {
-                Picker("Type", selection: $ui.displayType) {
-                    Text("All").tag(DisplayType.All)
-                    Text("Allowed").tag(DisplayType.Allowed)
-                    Text("Rejected").tag(DisplayType.Rejected)
-                }
-            })
-            ToolbarItem {
-                Toolbar()
-            }
-        }
     }
 }
 
